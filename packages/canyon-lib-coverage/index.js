c0462aebc23bbff8849f83141e2538c997231cad
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-03 19:11:24 +0800

    upd

---
+++ packages/canyon-lib-coverage/index.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import axios from "axios";
+import {checkCanyonFormat, mergeCoverageMap} from "./lib/helper.js";
+
+let initialCoverage = {}
+
+export const uploadCanyonCoverage = async ()=>{
+  // 1.检查是否存在.canyon_output文件夹
+  if (!fs.existsSync('./.canyon_output')) {
+    console.log('.canyon_output文件夹不存在')
+      return false
+  }
+
+  // 2.检查是否存在canyon.json文件
+  if (!fs.existsSync('./.canyon_output/canyon.json')) {
+    console.log('canyon.json文件不存在')
+      return false
+  }
+  const canyon = JSON.parse(fs.readFileSync('./.canyon_output/canyon.json', 'utf-8'))
+
+  // 3.检查canyon文件格式
+
+  if (!checkCanyonFormat(canyon)) {
+    console.log('canyon文件格式错误')
+      return false
+  }
+
+  const files = fs.readdirSync('./.canyon_output/')
+
+  let arr = []
+
+  files.forEach(file => {
+      if (file.includes('coverage-')) {
+        arr.push(file);
+        initialCoverage = mergeCoverageMap(initialCoverage, JSON.parse(fs.readFileSync(`./.canyon_output/${file}`, 'utf-8')));
+      }
+  })
+
+  console.log(`共发现${arr.length}个覆盖率文件`)
+
+
+  try {
+    fs.writeFileSync('./.canyon_output/coverage-final.json', JSON.stringify(initialCoverage, null, 2), 'utf-8')
+    return await axios.post(canyon.dsn.replace('https://','http://'), {
+      coverage: initialCoverage,
+      ...canyon,
+      reportID:'initial_coverage_data',
+    }, {
+      headers: {
+        Authorization: 'Bearer ' + canyon.reporter,
+      },
+      timeout: 15000,
+    }).then(res=>{
+      console.log(res.data)
+      return true
+    }).catch(err=>{
+      console.log('Failed to post coverage data:', err)
+      return false
+    })
+  } catch (e) {
+    console.log('Failed to post coverage data:', e)
+    return false
+  }
+}
